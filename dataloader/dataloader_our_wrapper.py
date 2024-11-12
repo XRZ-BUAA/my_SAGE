fix rotation

dataloader/dataloader_our_wrapper.py
@@ -136,7 +136,7 @@ def get_motion_list_wrapper(motion_lists):
 
 
 def get_data_stage1(dataset_path, split, protocol, **kwargs):
-    motion_list = get_path_stage1(dataset_path, split)
+    motion_list = get_path_stage1(dataset_path, split) # [:512]
     fnamelist = [
         '-'.join((i[:-len(Path(i).suffix)].split('/'))[-4:]) for i in motion_list
     ]
@@ -197,7 +197,7 @@ def get_motion_list_from_npydata_stage2(motion_npy:dict):
 def get_data_stage2(dataset_path, split, protocol, **kwargs):
     npypath = get_path_stage2(dataset_path, split)
     motion_npy = np.load(npypath, allow_pickle=True)
-    motion_list = get_motion_list_from_npydata_stage2(motion_npy)
+    motion_list = get_motion_list_from_npydata_stage2(motion_npy) # [:512]
     filenames = [f"test: {i}" for i in range(len(motion_list))]
     assert split in ['test', 'train', 'val']
     if split == 'test':

diffusion_stage/do_train_refiner.py
@@ -6,7 +6,8 @@ from accelerate import Accelerator
 import torch.nn.functional as F
 from test_refiner import test_process
 from utils.smplBody import BodyModel
-from utils import utils_transform
+# from utils import utils_transform
+from utils.transform_tools import rotation_6d_to_axis_angle
 
 lower_body = [0, 1, 2, 4, 5, 7, 8, 10, 11]
 upper_body = [0, 3, 6, 9, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21]
@@ -15,8 +16,8 @@ upper_body = [0, 3, 6, 9, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21]
 def calc_fk_loss(body_model, recover, gt, gt_pos):
     recover = recover.reshape(-1, 22, 6)
     gt = gt.reshape(-1, 22, 6)
-    pred_aa = utils_transform.sixd2aa(recover, batch=True).flatten(1, 2)
-    gt_aa = utils_transform.sixd2aa(gt, batch=True).flatten(1, 2)
+    pred_aa = rotation_6d_to_axis_angle(recover).flatten(1, 2)
+    gt_aa = rotation_6d_to_axis_angle(gt).flatten(1, 2)
     pred_loc = body_model({
         "root_orient": pred_aa[:, :3],
         "pose_body": pred_aa[:, 3:]